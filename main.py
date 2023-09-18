eb15bb109a84970e397e103e4142c199a6ee8b31
Izabela Szczecinska <izabelaszczecinska5@gmail.com>
2023-09-18 11:51:15 +0200

    Adding food on the board, which is changing its position to a new random position when  hit by a snake

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
+from food import Food
 import time
+
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,7 +11,7 @@ screen.title('Snake Game')
 screen.tracer(0)
 
 snake = Snake()
-
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, 'Up')
@@ -21,8 +23,11 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    # Collision with food
+    if snake.head.distance(food) < 15:
+        food.new_food_position()
 
 
 
